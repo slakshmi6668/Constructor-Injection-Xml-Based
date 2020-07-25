aabf1a6c1d578794cf741adc2fe3e4c868e258c0
S <sreelakshmi.s@dxc.com>
2020-07-25 11:07:40 +0530

    first commit

---
+++ src/main/java/org/dxc/ioc/App.java
@@ -0,0 +1,13 @@
+package org.dxc.ioc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
